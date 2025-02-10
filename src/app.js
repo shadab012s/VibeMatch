87f0a29ba692e4d25c21e7d7322f56d09aa2ebab
shadab012s <mdshadab0012s@gmail.com>
2025-02-10 19:58:05 +0530

    schema validation

--- src/app.js
+++ src/app.js
@@ -24,14 +24,70 @@ app.post("/signup",async (req,res)=>{
     res.send("user added sucessfully");
     }
     catch(err){
-        res.status(400).send("error saving the message");
+        res.status(400).send("error saving the message"+err.message);
     }
 
 
+});
+
+//getting user data with matched email
+app.get("/user", async(req,res)=>{
+const userEmail=req.body.email;
+
+try{
+    const users=await m1.find({email:userEmail});// it will send array of user with that email
+    if(users.length===0)
+        res.status(404).send("user not found");
+
+    res.send(users);
+}
+catch(err)
+{
+    res.status(400).send("something went wrong");
+}
+});
+
+app.get("/allUser",async(req,res)=>{
+    
+    const allUser=await m1.find({});
+    res.send(allUser);
 })
+app.delete("/deleteUser",async(req,res)=>{
+    const userID=req.body.id;
+    try{
+  const user= await m1.findByIdAndDelete(userID);
+  if(!user)
+    res.send("user not found");
+  res.send("user deleted Scucessfully");
+    }
+    catch(err){
+        res.status(404).send("somenthing went wrong");
+    }
+})
+app.patch("/updateUser",async(req,res)=>
+{
+    const id=req.body.id;
+    try{
+    const updatedUser=await m1.findByIdAndUpdate(id,req.body,
+        {returnDocument:"after",
+        runValidators:true,
+        });
+    
+    res.send({ message: "Updated successfully", user: updatedUser });
+
+    }
+    catch(err)
+    {
+        res.status(400).send("update failed"+err.message);
+    }
+    
+}
+
+)
 
 connectDB().then(()=>{
     console.log("database connected successfully");
+
     app.listen(7777,()=>{
         console.log("server is running suceesfully");
     });
--- src/models/user.js
+++ src/models/user.js
@@ -1,13 +1,62 @@
- const mongoose=require("mongoose");
-const {Schema}=mongoose;
-const userSchema= new Schema({
-    firsrname:String,
-    lastname:String,
-    age:Number,
-    email:String,
-    phoneNumber:Number,
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-});
-// model
-const userModel=mongoose.model("m1",userSchema);
-module.exports=userModel;
+const userSchema = new Schema({
+    firstname:{ 
+        type: String,  // Corrected field 
+        require:true,
+        minLength:4,
+        maxLength:50,
+    },
+    lastname: {
+        type: String,
+        minLength:3,
+        maxLength:40,
+
+    },
+    age: {
+        type:Number,
+        min:12,
+        max:70,
+        required:true,
+
+
+    },
+    email: {type:String,
+        unique:true,
+        required:true,
+        lowecase:true,
+        trim:true,
+    },
+    gender:{
+    type:String,
+    validate(value)
+    {
+        if(!["male","female","others"].includes(value))
+        {
+            throw new Error("gender data is not valid, only male , female and others allowed");
+        }
+    }
+    },
+    phoneNumber: Number,
+    photoUrl:{
+        type:String,
+        default:"https://www.pngegg.com/en/png-mfrjz"
+    },
+    skills:{
+        type:[String],
+    },
+    about:{
+        type:String,
+        default:"hey dev welcome to our platform"
+    }
+
+},
+{
+    timestamps:true
+}
+);
+
+// Model
+const userModel = mongoose.model("m1", userSchema);
+module.exports = userModel;
